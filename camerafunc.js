2f22a9e066f09591561b3a0d77574e4a58c281dd
Mikefila <74340408+Mikefila@users.noreply.github.com>
2024-02-01 00:50:09 -0500

    Update camerafunc.js
    Added all events
    Added individual rate limit for each event by camera

--- camerafunc.js
+++ camerafunc.js
@@ -1,20 +1,45 @@
 
 // isolate relevant incoming JSON
-var cameraName = msg.payload.event.DeviceName;
-var ivsRule = msg.payload.event.data.Name;
-var evco = msg.payload.event.Code;
-var action = msg.payload.event.action;
+this.cameraName = msg.payload.event.DeviceName;
+const ivsRule = msg.payload.event.data.Name;
+const evco = msg.payload.event.Code;
+const action = msg.payload.event.action;
+const index = msg.payload.event.index;
+
+// dahua events
+const validEvents = {
+    VideoMotion: "motion detection",
+    VideoLoss: "video loss detection",
+    VideoBlind: "video blind detection",
+    AlarmLocal: "alarm detection",
+    CrossLineDetection: "tripwire",
+    CrossRegionDetection: "intrusion",
+    LeftDetection: "abandoned object detection",
+    TakenAwayDetection: "missing object detection",
+    VideoAbnormalDetection: "scene change",
+    FaceDetection: "face detect",
+    AudioMutation: "intensity change",
+    AudioAnomaly: "input abnormal",
+    VideoUnFocus: "defocus detect",
+    WanderDetection: "loitering detection",
+    RioterDetection: "People Gathering",
+    ParkingDetection: "parking detection",
+    MoveDetection: "fast moving",
+    HeatImagingTemper: "temperature alarm"
+};
+// list of available incoming event codes
+const eventType = (Object.keys(validEvents));
 
 // message bounce 
+
 // create varibles for timestamp context stores
 // create camera variable
-var index = msg.payload.event.index; // returns camera number
-var cameraPx = "camera_"; 
-var camera = cameraPx + index; // create camera
+const cameraPx = "camera_"; 
+this.camera = cameraPx + index; // create camera
 
 
 // retrive timer if value does not exist set at 10000
-var cameraTest = (context.get(camera || 10000));
+var cameraTest = (context.get(this.camera || 10000));
 // create current time in ms
 var aTT = new Date().getTime();
 
@@ -25,90 +50,104 @@ if (aTT < cameraTest) {
 // if end of motion/ivs event message
 } else if (action === "Stop") {
     var aTTr = "Stop";
-} else {
+// event is not supported
+} else if (eventType.indexOf(evco) === -1) {
+    var attr = "event not upported";
 // if new time is greater than last camera event
+} else {
     var aTTr = "continue";
     var aTTd = (aTT + 7000); // add delay in milliseconds
     context.set(camera, aTTd);
 }
 
 // create empty message objects
-let msg1 = {};
-let msg2 = {};
-let msg3 = {};
+var msg1 = {};
+var msg2 = {};
+var msg3 = {};
 
 
-// create message if bounced
-if ((aTTr === "bounce") || (aTTr === "Stop")) {
+// create message if bounced output 3
+if (aTTr === "bounce") {
     msg1 = null;
     msg2 = null;
-    msg3.payload = camera; // output camera # if bounced
-} else {
+    msg3.payload = camera + " " + aTTr; // output camera # if bounced
+} else if (aTTr === "Stop") {
+    msg1 = null;
+    msg2 = null;
+    msg3.payload = camera + " " + aTTr;
+} else if (aTTr === "event not supported") {
+    msg1 = null;
+    msg2 = null;
+    msg3.payload = evco + " " + aTTr;
+} else { 
 // create message if passes
-    // phone state  ** used to stop tts while on phone  text still recieved
+    // phone state  ** used to stop tts while on phone  text still sent
     const phoneState = global.get('homeassistant.homeAssistant.states["sensor.pixel_7_phone_state_2"].state');
 
     // create entity type for service call
     const haDom = "camera";  // domain
     const serv = "snapshot"; // service
     const entityType = "camera."; // same as domain with period
-
+    // Dauha events
 
     // load snapshot count 
-    let countPx = "count_";
+    var countPx = "count_";
     // build countId for snapshots
-    let countId = countPx + camera;
+    var countId = countPx + camera + evco;
     // load current camera count
-    let countCc = context.get(countId) || 100;
+    var countCc = context.get(countId) || 100;
 
     // Build message 1 (msg1)
-    
+
     // Build entity
-    let cameraEntity = entityType + (cameraName.toLowerCase()).replace(/\s/g, "_") + "_main";
+    var cameraEntity = entityType + (cameraName.toLowerCase()).replace(/\s/g, "_") + "_main";
     // convert to lowercase  replace spaces with _   add entity type at begining   add _main to end
 
     // Text for title and message
-    var mtitle = ((cameraName.toUpperCase()).replace(/XVR /, "")).replace(/ FLOOR/, "");
+    var mTitle = ((cameraName.toUpperCase()).replace(/XVR /, "")).replace(/ FLOOR/, "");
     // convert to upper case  remove XVR and white space from beginning  remove whitespace and FLOOR from end
     // when present in title
 
+    // resolves event code for message text
+    var mEvent = validEvents[evco];
+
+    var mText = cameraName;
+
+    var mRule = "set";
+
+
+
+
     // rule 1
-    if (evco === "CrossRegionDetection" && action === "Start" && ivsRule === "ivs") {
-
+    if (evco === "CrossRegionDetection" && (ivsRule === "first" || ivsRule === "garden" || ivsRule === "alley" || ivsRule === "basement")) {
         countCc += 1; // if true add 1 to current count
-        msg1.payload = {
-            "domain": haDom, "service": serv, "data": { "entity_id": cameraEntity },
-            "driveFolder": cameraName, // storage folder
-            "title": mtitle,
-            "count": countCc,
-            "rule": "one", // this is for troubleshooting
-        };
-        // rule 2
-    } else if (evco === "CrossRegionDetection" && action === "Start" && (ivsRule === "first" || ivsRule === "garden" || ivsRule === "alley" || ivsRule === "basement")) {
+        cameraName = ivsRule; // storage folder
+        mTitle = ivsRule.toUpperCase();
+        mRule = "Rule one";
+        msg3.payload = mRule;
 
-        countCc += 1; // if true add 1 to current count
-        msg1.payload = {
-            "domain": haDom, "service": serv, "data": { "entity_id": cameraEntity },
-            "driveFolder": ivsRule, // storage folder
-            "title": ivsRule.toUpperCase(),
-            "count": countCc,
-            "rule": "two", // this is for troubleshooting
-        };
-        // rule 3
-    } else if (evco === "VideoMotion" && action === "Start") {
-
-        countCc += 1; // if true add 1 to current count 
-        msg1.payload = {
-            "domain": haDom, "service": serv, "data": { "entity_id": cameraEntity },
-            "driveFolder": cameraName, // storage folder
-            "title": mtitle,
-            "count": countCc,
-            "rule": "three", // this is for troubleshooting
-        };
-
+        // rule else
     } else {
-        msg1 = null;
-    }
+        countCc += 1; // if true add 1 to current count 
+        mRule = "Rule else";
+        msg3.payload = "else";
+
+    };
+
+    // message structure
+    msg1.payload = {
+        "domain": haDom,
+        "service": serv,
+        "target": {
+            "entity_id": cameraEntity
+        },
+        "driveFolder": cameraName, //.replace(/\s/g, "_"), // storage folder
+        "title": mTitle,
+        "event": mEvent,
+        "message": mText,
+        "count": countCc,
+        "rule": mRule, // this is for troubleshooting
+    };
 
     // reset current counter  allow 10 snapshots before overwrite
     if (countCc >= 110) {
@@ -120,21 +159,21 @@ if ((aTTr === "bounce") || (aTTr === "Stop")) {
 
     // build tts message and rules **output 2**
 
-    
+
     // if on phone, dont send tts 
     if (phoneState === "offhook" || action === "Stop") {
         msg2 = null;
     } else if (ivsRule === "ivs" || evco === "VideoMotion") {
         msg2.payload = ((cameraName.toLowerCase()).replace(/xvr /, "")).replace(/ floor/, "");
+    } else if (evco === "LeftDetection"){
+        msg2.payload = "Something is at the mailbox"
     } else {
         msg2.payload = ivsRule;
     }
 
-    msg3.payload = aTTr; //bounce test output
     //catch all if text output is null do nothing
     if (msg1 == null) {
         msg2 == null;
-        msg3 == null;
     }
 
 };
@@ -148,3 +187,4 @@ return [msg1, msg2, msg3];
 // message 3 to output 1
 // message 1 to output 2
 // message 2 to output 3
+
